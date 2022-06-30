d2e760480995147f691c0fa3f2921a252a2fd2e4
rda <2055017970@qq.com>
2022-06-30 18:58:47 +0800

    implement archlab

---
+++ archlab/ncopy.c
@@ -0,0 +1,24 @@
+typedef char word_t;
+
+word_t src[8], dst[8];
+
+/* $begin ncopy */
+/*
+ * ncopy - copy src to dst, returning number of positive ints
+ * contained in src array.
+ */
+word_t ncopy(word_t *src, word_t *dst, word_t len)
+{
+    word_t count = 0;
+    word_t val;
+
+    while (len > 0) {
+	val = *src++;
+	*dst++ = val;
+	if (val > 0)
+	    count++;
+	len--;
+    }
+    return count;
+}
+/* $end ncopy */
